1760373a9416038705c009c9fafb2be8d070ed2a
Tolamo Antti <antti.tolamo@digia.com>
2021-08-04 16:27:55 +0300

    added form and post json examples. Also added nodemon to restart server if code is changed

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 3000
 const mariadb = require('mariadb');
 const {networkInterfaces} = require('os')
 
+
+//these two are needed for form data to be parsed
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const nets = networkInterfaces();
 const results = Object.create(null); // Or just '{}', an empty object
 
@@ -21,12 +26,12 @@ for (const name of Object.keys(nets)) {
 }
 //ottaa id:n ja palauttaa id:n
 app.get('/api/work_site', (req, res) => {
-    res.send('Hello World!' + req.query.id)
+    res.send('Hei maailma' + req.query.id)
 })
 
 //esimerkki json vastauksesta
 app.get('/api/work_site/json', (req, res) => {
-    const jsonAnswer = {test: "jotain tekstiä", id: 1}
+    const jsonAnswer = {test: "jotain tekstiä", id: req.query.id}
     res.json(jsonAnswer)
 })
 
@@ -37,8 +42,41 @@ app.get('/api/work_site/database', async (req, res) => {
     res.json(rows)
 })
 
+//Haetaan käyttäjä id:n perusteella
+app.get('/api/user/', async (req, res) => {
+    const rows = await asyncFunction("SELECT * FROM user WHERE user_id=" + req.query.id)
+    res.json(rows)
+})
+
+//Haetaan työalueet työalue-id:n perusteella
+app.get('/api/work_area/', async (req, res) => {
+    const rows = await asyncFunction("SELECT * FROM work_area WHERE work_area_id=" + req.query.id)
+    res.json(rows)
+})
+
+//Haetaan työalueet työalue-id:n perusteella
+app.get('/api/work_task/', async (req, res) => {
+    const rows = await asyncFunction("SELECT * FROM work_task WHERE work_task_id=" + req.query.id)
+    res.json(rows)
+})
+
+//json example how to get from POST
+app.post('/postjsonexample', express.json({type: '*/*'}), async (req, res) => {
+    console.log(JSON.stringify(req.body)) //to get whole body JSON
+    console.log(JSON.stringify(req.body.test)) //to get value from JSON like {"test":1,"test2":"something"}
+    res.json(req.body)
+})
+
+//form example how to get form
+app.post('/postjformexample',  async (req, res) => {
+    console.log(req.body) //to get whole body JSON
+    console.log(JSON.stringify(req.body.test)) //to get value from JSON like {"test":1,"test2":"something"}
+    res.json(req.body)
+})
+
+
 //asynce function because database calls are async: they come back when they are ready
-async function asyncFunction() {
+async function asyncFunction(querySentence) {
     return await mariadb
         .createConnection({ //set database connection
             host: 'localhost',
@@ -46,13 +84,14 @@ async function asyncFunction() {
             password: 'test',
             database: 'test',
         }).then(connection => {
-            return connection.query("SELECT * FROM test LIMIT 1") //get row from database return call above
+            return connection.query(querySentence) //get row from database return call above
                 .then(rows => {
                     console.log(rows)
                     return rows //when ready return to previous return rows
                 })
                 .catch(err => {
                     console.log('Error is:'+ JSON.stringify(err))
+                    return "Error"
                 });
         })
 
